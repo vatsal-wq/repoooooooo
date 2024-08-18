b156f0a9738c670b4cae12cf198a7fdce8949170
vatsal-wq <51950758+vatsal-wq@users.noreply.github.com>
2024-08-18 23:36:11 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+import Home from './components/Home/Home';
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/" element={<Login />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
